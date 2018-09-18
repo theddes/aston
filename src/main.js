07b3ebb9f253c92b77b623200a95a83a9cf07bd8
theddes <timoheddes@gmail.com>
2018-09-18 15:38:16 +0200

    Optimized Storyblok integration

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
+
+// plugins
 import StoryblokVue from 'storyblok-vue'
+import Img from './plugins/img'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import './registerServiceWorker'
 
 Vue.use(StoryblokVue)
+Vue.use(Img)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,37 +9,13 @@
 </template>
 
 <script>
-import api from '../utils/api'
+import storyblokMixin from '../mixins/storyblok'
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  data () {
-    return {
-      content: null
-    }
-  },
-  created () {
-    this.fetchData()
-    if (this.$storyblok) {
-      this.$storyblok.init()
-
-      // Reload the browser if the content
-      // is saved or published in the editor.
-      this.$storyblok.on(['change', 'published'], () => window.location.reload())
-
-      // Live update the content
-      // in the Visual Editor.
-      this.$storyblok.on('input', ({ story }) => {
-        this.content = story.content
-      })
-    }
-  },
-  methods: {
-    async fetchData () {
-      const { data } = await api.get(`cdn/stories/${this.$options.name.toLowerCase()}`)
-      this.content = data.story.content
-    }
-  }
+  mixins: [
+    storyblokMixin
+  ]
 }
 </script>
